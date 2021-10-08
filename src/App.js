cc78688a825c98c0debe2b617d6c27c8f8eedbd1
Kevin <meaj.mailbox@gmail.com>
2021-10-08 17:32:03 -0500

    Completed Element Deletion added ability to remove todos from list

--- src/App.js
+++ src/App.js
@@ -9,20 +9,51 @@ function TodoList({ todos, setTodos }) {
     setTodos(updatedTodos);
   }
 
+  if (!todos.length) {
+    return <p>No todos left!</p>;
+  }
+
   return (
     <ul>
       {todos.map((todo) => (
         <li
-          onClick={() => handleToggleTodo(todo)}
+          onDoubleClick={() => handleToggleTodo(todo)}
           style={{
             textDecoration: todo.done ? "line-through" : ""
           }}
           key={todo.id}
         >
           {todo.text}
+          <DeleteTodo todo={todo} setTodos={setTodos} />
         </li>
       ))}
     </ul>
+  );
+}
+
+function DeleteTodo({ todo, setTodos }) {
+  function handleDeleteTodo() {
+    const confirmed = window.confirm("Do you want to delete this?");
+    if (confirmed) {
+      setTodos((prevTodos) => {
+        return prevTodos.filter((prev) => prev.id !== todo.id);
+      });
+    }
+  }
+
+  return (
+    <span
+      onClick={handleDeleteTodo}
+      role="button"
+      style={{
+        color: "red",
+        frontWeight: "bold",
+        marginLeft: 10,
+        curson: "pointer"
+      }}
+    >
+      x
+    </span>
   );
 }
 
